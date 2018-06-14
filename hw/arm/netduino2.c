de32fe935468aae9f2f65d35601f8b3c9016a850
markok314 <markok3.14@gmail.com>
2018-06-14 16:28:54 +0200

    solved error on boot for netduino

--- hw/arm/netduino2.c
+++ hw/arm/netduino2.c
@@ -25,6 +25,7 @@
 #include "qemu/osdep.h"
 #include "qapi/error.h"
 #include "hw/boards.h"
+#include "hw/loader.h"
 #include "qemu/error-report.h"
 #include "hw/arm/stm32f205_soc.h"
 #include "hw/arm/arm.h"
@@ -39,6 +40,40 @@ static void netduino2_init(MachineState *machine)
 
     armv7m_load_kernel(ARM_CPU(first_cpu), machine->kernel_filename,
                        FLASH_SIZE);
+
+    /* The folowing two calls are a workaround for problem with rom in aliased
+     * addresses on STM32. It is also possible to call qemu_system_reset()
+     * twice in main(), but then we modify code for all targets. This solution
+     * registers rom reset twice, but the change is local to STM32.
+     * For investigation set breakpoints in:
+     * - armv7m.c [line: 261]	- cpu_reset(CPU(cpu));
+     * - armv7m.c [line: 281]   - armv7m_load_kernel(ARM_CPU(first_cpu), kernel_filename, mem_size);
+     * - cpu.c [line: 235]      - SP and PC are loaded: initial_msp = ldl_phys(s->as, vecbase);
+     * - loader.c [line: 1070]  - rom_insert(rom);
+     * - loader.c [line: 1095]  - rom_reset() { ... }
+     * - loader.c [line: 1119]	- rom_check_and_register_reset()
+     *
+     * Description:
+     * Kernel is loaded into Rom objects, which are stored in loader.c. Only
+     * in
+     * void rom_reset() {
+     *   ...
+     *    cpu_physical_memory_write_rom(rom->as, rom->addr, rom->data,
+     *                                  rom->datasize);
+     *   ...
+     * }
+     * are Roms copied to memory. However, since CPU reset is registered BEFORE
+     * rom_reset(), SP and PC are read from memory BEFORE roms are copied to
+     * memory. CPU reset also checks Roms for data, so this is normally not a
+     * problem, but in STM32 PC and SP are read from aliased memory, and these
+     * memory addresses are not mapped to Roms. Since ROMs are private in
+     * loader.c the only option to copy Roms to memory is through reset. So we
+     * first have to register Roms for reset, and then trigger system reset, which
+     * copies Roms to mem, so that reset vectors are in memory, when
+     * qemu_system_reset() is called again from main()
+     */
+    rom_check_and_register_reset();
+    qemu_system_reset(SHUTDOWN_CAUSE_NONE);
 }
 
 static void netduino2_machine_init(MachineClass *mc)
