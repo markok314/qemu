496d9d3748b7c0420a6d7a12d6cabad7301f3719
markok314 <markok3.14@gmail.com>
2018-06-29 17:08:21 +0200

    code relocated from 0x100 to to 0x0

--- isystem/samples/rh850/build.sh
+++ isystem/samples/rh850/build.sh
@@ -8,4 +8,4 @@ fi
 
 wine ~/bin/iar/bin/iasmrh850.exe $1.s
 wine ~/bin/iar/bin/ilinkrh850.exe --no_remove --vfe -o$1.elf --map \
- $1.map --config_def CSTACK_SIZE=0x1000 --config_def HEAP_SIZE=4096 $1.o
+ $1.map --config lnkrh850.icf --config_def CSTACK_SIZE=0x1000 --config_def HEAP_SIZE=4096 $1.o
