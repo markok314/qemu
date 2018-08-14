11b4c798a3949650eadb5facfbeb2fc39be23352
robertKalem <robert.kalem@gmail.com>
2018-08-14 15:46:53 +0200

    edited python testing script

--- isystem/samples/rh850/test.py
+++ isystem/samples/rh850/test.py
@@ -2,20 +2,24 @@ import sys
 import os
 import time
 
+# Run this script in qemu/isystem/samples/rh850/ with test file without extention and number of instructions as arguments
+# Example: python3 ./test.py test_file 10
+
 if len(sys.argv)<3:
     print('Arguments missing!')
     print('Usage: python3 test.py name_of_test_file_without_extensions numbers_of_instructions')
     exit()
-if os.path.getmtime(sys.argv[1]+'.elf') < os.path.getmtime(sys.argv[1]+'.s'):
-    os.system('./build.sh '+sys.argv[1])
 
-os.system('../../../rh850-softmmu/qemu-system-rh850 -M rh850mini -s -singlestep -d nochain,exec,in_asm,cpu -D rh850.log -kernel '+sys.argv[1]+'.elf &')
-time.sleep(1)
+os.system('./build.sh '+sys.argv[1])
+
+os.system('../../../rh850-softmmu/qemu-system-rh850 -M rh850mini -s -singlestep -d nochain,exec,in_asm,cpu -D ../../../rh850.log -kernel '+sys.argv[1]+'.elf &')
+time.sleep(3)
 os.system('kill $(pgrep qemu-system)')
 stevilo = int(sys.argv[2]) + 31
 
-os.system('python3 checkRegistersBlueBox.py ' + sys.argv[1] + ' ' + str(stevilo) + ' > log1.log')
 os.system('python3 checkRegisters.py ../../../rh850.log ' + str(stevilo) + ' > log2.log')
+os.system('python3 checkRegistersBlueBox.py ' + sys.argv[1] + ' ' + str(stevilo) + ' > log1.log')
+
 
 f1 = open("log2.log", "r")
 f2 = open("log1.log", "r")
@@ -24,6 +28,7 @@ index = 0
 start = 0;
 okay = True
 
+print("----------")
 for line1, line2 in zip(f1, f2):
     if start>=35*31:
         if(index == 0):
