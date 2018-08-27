a56a35afea8bad57b15c0c2622f1c4413bcb0b72
markok314 <markok3.14@gmail.com>
2018-08-27 15:46:04 +0200

    added examples for movea

--- isystem/samples/rh850/gcc/test-gcc.s
+++ isystem/samples/rh850/gcc/test-gcc.s
@@ -1,8 +1,20 @@
+.data
+lbl1: .word 57
+lbl2: .byte 89
+
 .text
 
    # prepare r20, 0
    mov     r6, r20
    movea   0x0014, r0, r7
+
+   # this works as expected
+   mov     .data, r0
+   movea   lbl2 - .data, r0, r7
+
+   movea   lblX, r0, r7 # does not compile correctly with GCC, linker
+                        # adds imm16 also to op code: 38 3e 18 00 instead 
+                        # of 02 3e 18 00
    mov     10, r6
    add     r6, r7
 lblX:
