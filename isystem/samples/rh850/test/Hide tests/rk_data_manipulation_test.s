6b957b630c6c49c37f8d51acaf88e461cf28ab4c
robertKalem <robert.kalem@gmail.com>
2018-09-11 12:46:23 +0200

    added tests to rk_data_manipulation.s

--- isystem/samples/rh850/test/Hide tests/rk_data_manipulation_test.s
+++ isystem/samples/rh850/test/Hide tests/rk_data_manipulation_test.s
@@ -87,16 +87,277 @@
 
 #----------------------Testing the SAR instruction-----
 
-
-
+	sar r5, r1, r7		# CY and S flag are set
+	sar r5, r2, r8
+	sar r0, r1, r9		# S flag is set
+	sar r0, r2, r10		
+	sar 0x6, r4			
+	sar 0xa, r1		# CY and S flag are set
+	sar 0x4, r6		# Z flag is set
+	sar r6, r3
 
 #----------------------Testing the SASF instruction-----
 
+			# Testing with no flag set
+	sasf V, r1	
+	sasf S, r2
+	sasf NV, r3
+	sasf NS, r4
+	sasf C, r5
+	sasf T, r6
+	sasf NC, r1
+	sasf SA, r2
+	sasf Z, r3
+	sasf LT, r4
+	sasf NZ, r5
+	sasf GE, r6
+	sasf NH, r1
+	sasf LE, r2
+	sasf H, r3
+	sasf GT, r4
+
+	cmp r1, r2	# Testing with CY, OV and S flags set
+
+	sasf V, r1
+	sasf S, r2
+	sasf NV, r3
+	sasf NS, r4
+	sasf C, r5
+	sasf T, r6
+	sasf NC, r1
+	sasf SA, r2
+	sasf Z, r3
+	sasf LT, r4
+	sasf NZ, r5
+	sasf GE, r6
+	sasf NH, r1
+	sasf LE, r2
+	sasf H, r3
+	sasf GT, r4
+
+	cmp r2, r1	# Testing with CY and S flags set
+
+	sasf V, r1
+	sasf S, r2
+	sasf NV, r3
+	sasf NS, r4
+	sasf C, r5
+	sasf T, r6
+	sasf NC, r1
+	sasf SA, r2
+	sasf Z, r3
+	sasf LT, r4
+	sasf NZ, r5
+	sasf GE, r6
+	sasf NH, r1
+	sasf LE, r2
+	sasf H, r3
+	sasf GT, r4
+
+	cmp r3, r3	# Testing with Z flag set
+
+	sasf V, r1
+	sasf S, r2
+	sasf NV, r3
+	sasf NS, r4
+	sasf C, r5
+	sasf T, r6
+	sasf NC, r1
+	sasf SA, r2
+	sasf Z, r3
+	sasf LT, r4
+	sasf NZ, r5
+	sasf GE, r6
+	sasf NH, r1
+	sasf LE, r2
+	sasf H, r3
+	sasf GT, r4
+	
+	mov 0x7fffffff, r10
+	satadd 0x1, r10		# Testing with SA flag set
+
+	sasf V, r1
+	sasf S, r2
+	sasf NV, r3
+	sasf NS, r4
+	sasf C, r5
+	sasf T, r6
+	sasf NC, r1
+	sasf SA, r2
+	sasf Z, r3
+	sasf LT, r4
+	sasf NZ, r5
+	sasf GE, r6
+	sasf NH, r1
+	sasf LE, r2
+	sasf H, r3
+	sasf GT, r4
+
+	mov 0x80000000, r10
+	sub r6, r10		# Testing with OV flag set
+
+	sasf V, r1
+	sasf S, r2
+	sasf NV, r3
+	sasf NS, r4
+	sasf C, r5
+	sasf T, r6
+	sasf NC, r1
+	sasf SA, r2
+	sasf Z, r3
+	sasf LT, r4
+	sasf NZ, r5
+	sasf GE, r6
+	sasf NH, r1
+	sasf LE, r2
+	sasf H, r3
+	sasf GT, r4
+
 #----------------------Testing the SETF instruction-----
+
+
+	cmp r2, r1	# Testing with CY and S flags set
+
+	setf V, r1
+	setf S, r2
+	setf NV, r3
+	setf NS, r4
+	setf C, r5
+	setf T, r6
+	setf NC, r1
+	setf SA, r2
+	setf Z, r3
+	setf LT, r4
+	setf NZ, r5
+	setf GE, r6
+	setf NH, r1
+	setf LE, r2
+	setf H, r3
+	setf GT, r4
+
+			# Testing with no flag set
+	setf V, r1
+	setf S, r2
+	setf NV, r3
+	setf NS, r4
+	setf C, r5
+	setf T, r6
+	setf NC, r1
+	setf SA, r2
+	setf Z, r3
+	setf LT, r4
+	setf NZ, r5
+	setf GE, r6
+	setf NH, r1
+	setf LE, r2
+	setf H, r3
+	setf GT, r4
+
+	cmp r3, r3	# Testing with Z flag set
+
+	setf V, r1
+	setf S, r2
+	setf NV, r3
+	setf NS, r4
+	setf C, r5
+	setf T, r6
+	setf NC, r1
+	setf SA, r2
+	setf Z, r3
+	setf LT, r4
+	setf NZ, r5
+	setf GE, r6
+	setf NH, r1
+	setf LE, r2
+	setf H, r3
+	setf GT, r4
+	
+	mov 0x7fffffff, r10
+	satadd 0x1, r10		# Testing with SA flag set
+
+	setf V, r1
+	setf S, r2
+	setf NV, r3
+	setf NS, r4
+	setf C, r5
+	setf T, r6
+	setf NC, r1
+	setf SA, r2
+	setf Z, r3
+	setf LT, r4
+	setf NZ, r5
+	setf GE, r6
+	setf NH, r1
+	setf LE, r2
+	setf H, r3
+	setf GT, r4
+
+	mov 0x80000000, r10
+	sub r6, r10		# Testing with OV flag set
+
+	setf V, r1
+	setf S, r2
+	setf NV, r3
+	setf NS, r4
+	setf C, r5
+	setf T, r6
+	setf NC, r1
+	setf SA, r2
+	setf Z, r3
+	setf LT, r4
+	setf NZ, r5
+	setf GE, r6
+	setf NH, r1
+	setf LE, r2
+	setf H, r3
+	setf GT, r4
+
+	cmp r1, r2	# Testing with CY, OV and S flags set
+
+	setf V, r1
+	setf S, r2
+	setf NV, r3
+	setf NS, r4
+	setf C, r5
+	setf T, r6
+	setf NC, r1
+	setf SA, r2
+	setf Z, r3
+	setf LT, r4
+	setf NZ, r5
+	setf GE, r6
+	setf NH, r1
+	setf LE, r2
+	setf H, r3
+	setf GT, r4
 
 #----------------------Testing the SHL instruction-----
 
+	mov 0x80000000, r1
+	mov 0x7fffffff, r2
+	mov 0x0acdef00, r3
+	mov 0x6, r4
+	mov 0x1, r5
+
+
+	shl r5, r1, r6		# CY and Z flags are set
+	shl r5, r2, r7		# S flag is set
+	shl r0, r1, r8		# S flag is set
+	shl r0, r2, r8
+	shl r4, r0, r9		# Z flag is set
+	shl r4, r3		# S flag is set
+	shl r3, r3		# CY flag is set
+
+
 #----------------------Testing the SHR instruction-----
+
+	mov 0x1, r1
+	mov 0x2, r2
+	
+	shr 0x0, r1
+	shr 0x1, r1
+	shr 0x0, r2
+	shr 0x2, r2
 
 
 	ldsr r0, psw
