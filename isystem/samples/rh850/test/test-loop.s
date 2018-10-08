41fc2272291d32e0c496eae552cdc7f3a07a5e8e
Branko Šter <branko.ster@fri.uni-lj.si>
2018-10-08 17:31:43 +0200

    LOOP finished

--- isystem/samples/rh850/test/test-loop.s
+++ isystem/samples/rh850/test/test-loop.s
@@ -2,14 +2,15 @@
 
 .text
 
-	mov 1, R1
+	mov 1, R4
+	mov 3, R1
 	mov 5, R2
 	add R1, R2
-	add R2, R2
-	mov 4, R3
-	#jmp R3
-	mov 5, R2
-	LOOP R1, -8
-	add R2, R2
-	add R2, R2
-	add R2, R2
+	sub R4, R1
+	loop R1, 6
+	mov 4, R2
+	add R2, R5
+	sub R4, R2
+	loop R2, 4
+	mov 3, R2
+	mov 4, R2
