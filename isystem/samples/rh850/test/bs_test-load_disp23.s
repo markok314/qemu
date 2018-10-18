ced91f2db9e24746cbb8a401fde9cd4e987c643a
Branko Šter <branko.ster@fri.uni-lj.si>
2018-10-18 17:53:51 +0200

    .

--- isystem/samples/rh850/test/bs_test-load_disp23.s
+++ isystem/samples/rh850/test/bs_test-load_disp23.s
@@ -1,5 +1,3 @@
-.define
-
 .data
 var1:   .space  5
 var2:   .byte   7
@@ -10,8 +8,6 @@ start = 0xfee00000
 
 .text
         mov start, r1
-
-.ifdef LD.B_2
 
 #----------------------------------ld.b(2)--disp23---
         movea var1 - .data, r1, r2
@@ -33,6 +29,22 @@ start = 0xfee00000
         ld.b 0x5555[r2], r7
 
 #----------------------------------ld.bu(2)--disp23---
+
+		mov 12, r3
+        st.b r3, 0[r2]
+        ld.bu 0[r2], r4
+
+		mov 0x78, r3
+        st.b r3, 0xffff[r2]
+        ld.bu 0xffff[r2], r5
+
+		mov 0xab, r3
+        st.b r3, 0x3ffff[r2]
+        ld.bu 0x3ffff[r2], r6
+
+		mov -11, r3
+        st.b r3, 0x5555[r2]
+        ld.bu 0x5555[r2], r7
 
 
 #----------------------------------ld.h(2)--disp23---
