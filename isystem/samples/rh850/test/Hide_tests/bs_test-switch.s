5e003d97e12abea8dbfb8fc21cd75354ca908d6d
Branko Šter <branko.ster@fri.uni-lj.si>
2018-11-19 14:58:56 +0100

    switch tested

---
+++ isystem/samples/rh850/test/Hide_tests/bs_test-switch.s
@@ -0,0 +1,18 @@
+.text
+
+#------SWITCH---------------------------- OK
+
+		mov 7, r1
+		switch r1
+		mov 1, r2
+		mov 2, r2
+		mov 4, r1
+		switch r1
+		mov 3, r2
+		mov 4, r2
+		br 6
+LUT:	.hword	2, 0
+		mov 5, r2
+		mov 6, r2
+		nop
+
