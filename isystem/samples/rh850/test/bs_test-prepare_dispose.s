11eef25df924dbe0fc45327e38f3ec46c62d68ce
Branko Šter <branko.ster@fri.uni-lj.si>
2019-02-06 13:05:48 +0100

    some corrections (brane)

--- isystem/samples/rh850/test/bs_test-prepare_dispose.s
+++ isystem/samples/rh850/test/bs_test-prepare_dispose.s
@@ -1,11 +1,10 @@
-#.include "RH850G3M_insts.s"
+.include "RH850G3M_insts.s"
 
-.data
 stack:		.word 0, 0, 0, 0, 0 	# last 0 has address stack+16
 
 start = 0xfee00000
 
-.text
+.include "gpr_init.s"
 
         mov start, r1
         movea stack - .data, r1, r2
@@ -18,5 +17,25 @@ start = 0xfee00000
 		ld.w 4[sp], r4
 		ld.w 8[sp], r5
 
-		#DISPOSE imm5, list12
+		DISPOSE 1, 0x401
+		nop
+
+		mov 1, r20
+		mov 2, r21
+		mov 3, r22
+		mov 4, r23
+		mov 5, r24
+		mov 6, r25
+		mov 7, r26
+		mov 8, r27
+		mov 9, r28
+		mov 10, r29
+		mov 11, r30
+		mov 12, r31
+		PREPARE 0x401, 0x10
+		DISPOSE 0x10, 0x401
+		nop
+
+Lbl:	BR Lbl
+
 
