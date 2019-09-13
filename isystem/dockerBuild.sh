be22672eb793cba5509921779efaf35b824a2012
markok314 <markok3.14@gmail.com>
2019-09-13 12:42:41 +0200

    build works now

--- isystem/dockerBuild.sh
+++ isystem/dockerBuild.sh
@@ -28,7 +28,8 @@ for prefix in x86_64-w64-mingw32-; do
         --enable-vnc \
         --enable-bzip2 \
         --enable-guest-agent \
-        --with-sdlabi=2.0
+        --with-sdlabi=2.0 \
+        --disable-debug-info
 done
 
 echo "===================%%%%%%%%%%%%%%%%%%%%%%%==================="
@@ -36,30 +37,39 @@ EXE_DIR=/tmp/qemu-test/build
 DIST_DIR=/var/tmp/qemu/dist
 mkdir $DIST_DIR
 cp /usr/x86_64-w64-mingw32/sys-root/mingw/bin/*.dll $DIST_DIR
-if [[ $TARGET_LIST == *"arm"* ]]; then
-    cp $EXE_DIR/arm-softmmu/qemu-system-arm.exe $EXE_DIR/arm-softmmu/qemu-system-armw.exe $DIST_DIR
-fi
 
-if [[ $TARGET_LIST == *"aarch64"* ]]; then
-    cp $EXE_DIR/aarch64-softmmu/qemu-system-aarch64.exe $EXE_DIR/aarch64-softmmu/qemu-system-aarch64w.exe $DIST_DIR
-fi
+executables=(arm aarch64 ppc ppc64 tricore rh850)
+for exeprefix in "${executables[@]}"; do
+    if [[ $TARGET_LIST == *"$exeprefix"* ]]; then
+#        cp $EXE_DIR/$exeprefix-softmmu/qemu-system-$exeprefix.exe $DIST_DIR
+        cp $EXE_DIR/$exeprefix-softmmu/qemu-system-${exeprefix}w.exe $DIST_DIR
+    fi
+done
 
-if [[ $TARGET_LIST == *"ppc"* ]]; then
-    cp $EXE_DIR/ppc-softmmu/qemu-system-ppc.exe $EXE_DIR/ppc-softmmu/qemu-system-ppcw.exe $DIST_DIR
-fi
-
-if [[ $TARGET_LIST == *"ppc64"* ]]; then
-    cp $EXE_DIR/ppc64-softmmu/qemu-system-ppc64.exe $EXE_DIR/ppc64-softmmu/qemu-system-ppc64w.exe $DIST_DIR
-fi
-
-if [[ $TARGET_LIST == *"tricore"* ]]; then
-    cp $EXE_DIR/tricore-softmmu/qemu-system-tricore.exe $EXE_DIR/tricore-softmmu/qemu-system-tricorew.exe $DIST_DIR
-fi
-
-if [[ $TARGET_LIST == *"rh850"* ]]; then
-    cp $EXE_DIR/rh850-softmmu/qemu-system-rh850.exe $EXE_DIR/rh850-softmmu/qemu-system-rh850w.exe $DIST_DIR
-fi
-
+#if [[ $TARGET_LIST == *"arm"* ]]; then
+#    cp $EXE_DIR/arm-softmmu/qemu-system-arm.exe $EXE_DIR/arm-softmmu/qemu-system-armw.exe $DIST_DIR
+#fi
+#
+#if [[ $TARGET_LIST == *"aarch64"* ]]; then
+#    cp $EXE_DIR/aarch64-softmmu/qemu-system-aarch64.exe $EXE_DIR/aarch64-softmmu/qemu-system-aarch64w.exe $DIST_DIR
+#fi
+#
+#if [[ $TARGET_LIST == *"ppc"* ]]; then
+#    cp $EXE_DIR/ppc-softmmu/qemu-system-ppc.exe $EXE_DIR/ppc-softmmu/qemu-system-ppcw.exe $DIST_DIR
+#fi
+#
+#if [[ $TARGET_LIST == *"ppc64"* ]]; then
+#    cp $EXE_DIR/ppc64-softmmu/qemu-system-ppc64.exe $EXE_DIR/ppc64-softmmu/qemu-system-ppc64w.exe $DIST_DIR
+#fi
+#
+#if [[ $TARGET_LIST == *"tricore"* ]]; then
+#    cp $EXE_DIR/tricore-softmmu/qemu-system-tricore.exe $EXE_DIR/tricore-softmmu/qemu-system-tricorew.exe $DIST_DIR
+#fi
+#
+#if [[ $TARGET_LIST == *"rh850"* ]]; then
+#    cp $EXE_DIR/rh850-softmmu/qemu-system-rh850.exe $EXE_DIR/rh850-softmmu/qemu-system-rh850w.exe $DIST_DIR
+#fi
+#
 echo "BUILD FINISHED"
 # /bin/bash
 
--- isystem/utils/createWindowsDistro.py
+++ isystem/utils/createWindowsDistro.py
@@ -12,6 +12,9 @@ import shutil
 g_isVerbose = False
 BIN_DIR = 'build'
 DOCKER_DIR_GLOB = 'docker-src*'
+DIST_DIR = 'dist'
+ZIP_FILE_NAME = 'qemu-isystem'
+ZIP_FILE_EXT = 'zip'
 
 def log(msg: str):
     if g_isVerbose:
@@ -48,7 +51,7 @@ def create_host_dir():
     return list(newDockerDir)[0]
 
 
-def start_docker(docker_host_dir, targets):
+def _docker_build(docker_host_dir, targets):
 
     isVerbose = 0
     if g_isVerbose:  # Option: V=1 for verbose build
@@ -61,140 +64,135 @@ def start_docker(docker_host_dir, targets):
 
     log('Start docker for build and copy: ' + cmd)
 
-    p = subprocess.Popen(cmd, shell=True)
+    p = subprocess.check_call(cmd, shell=True)
 
-    while True:
-        time.sleep(1)
-        id = str(os.popen("sudo docker ps --filter ancestor=qemu:fedora -q").readlines())
-        if len(id) > 4:
-            id = id[2:]
-            id = id[:-4]
-            log(f'Started docker with id: {id}')
-            break
-
-    return id
-
-
-def stop_docker():
-    os.chdir('..')
-    log(os.getcwd())
-
-    cmd = ["sudo docker stop " + id]
-    log('Stop docker: ' + ' '.join(cmd))
-    p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
-
-    p.wait()
-
-    # remove docker
-    cmd = ['sudo rm -rf docker-src*']
-    p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
-
-    os.chdir('isystem')
+#    while True:
+#        time.sleep(1)
+#        id = str(os.popen("sudo docker ps --filter ancestor=qemu:fedora -q").readlines())
+#        if len(id) > 4:
+#            id = id[2:]
+#            id = id[:-4]
+#            log(f'Started docker with id: {id}')
+#            break
+#
+#    return id
 
 
-def check_dlls(arch: str, targets):
+def _check_dlls(out_dir, arch: str, targets):
     """
     This f. checks, which dlls are really necessary for running quemu
     as simulator for isystem.test.
     """
 
-    os.chdir(f'{BIN_DIR}{arch}')
+    os.chdir(os.path.join(out_dir, DIST_DIR))
 
-    if os.path.exists("filtered_dlls"):
-        shutil.rmtree("filtered_dlls")
-    os.makedirs("filtered_dlls")
-
-    os.chdir(f'dlls{arch}')
+    REDUNDANT_DIR = '../redundantDlls'
+    # shutil.rmtree(REDUNDANT_DIR)
+    if not os.path.exists(REDUNDANT_DIR):
+        os.mkdir(REDUNDANT_DIR)
+    
 
     for dllFName in glob.glob("*.dll"):
         os.rename(dllFName, dllFName + '_tst')
-        print(dllFName, end='')
         try:
-
-            cmd = ["su -c 'wine qemu-system-" + check + ".exe -M help > /dev/null 2>&1 '"]
-            log('check_dlls: ' + ' '.join(cmd))
-            subprocess.check_call(cmd, shell=True, executable='/bin/bash')
-            print()
+            #for target in targets:
+            target = 'arm'
+            cmd = "wine qemu-system-" + target + "w.exe -M help > /dev/null 2>&1 "
+            subprocess.check_call(cmd, shell=True)
+            log('Redundant dll: ' + dllFName)
             os.rename(dllFName + '_tst', dllFName)
+            shutil.move(dllFName, REDUNDANT_DIR)
         except:
-            print(" NEEDED")
+            log('Required dll: ' + dllFName)
             os.rename(dllFName + '_tst', dllFName)
-            shutil.copy2(dllFName, '../filtered_dlls')
 
-    os.chdir("..")
-
-
-def zip_files():
-    if os.path.exists(f'{BIN_DIR}32'):
-        shutil.make_archive("qemu-i686", 'zip', f'{BIN_DIR}32')
-    if os.path.exists(f'{BIN_DIR}64'):
-        shutil.make_archive("qemu-x64", 'zip', f'{BIN_DIR}64')
+    os.chdir("../..")
 
 
+def _zip_files(out_dir):
+    log("Zipping ...")
+    shutil.make_archive(ZIP_FILE_NAME, ZIP_FILE_EXT, os.path.join(out_dir, DIST_DIR))
 
 
-def _copyFromDockerToSharedDir(id, arch, targets):
-    """
-    :param arch: architecture, should be '32' or '64'
-    :param targets: list of strings for targets, eg. ['arm', 'ppc']
-    """
-
-    for target in targets:
-        cmd = [f"sudo docker cp {id}:tmp/qemu-test/src/{target}-softmmu/qemu-system-{target}.exe"
-                                        f" {BIN_DIR}{arch}"]
-        p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
-        p.wait()
-
-
-def _dockerBuild(dockerId, arch: str, targets):
-    """
-    :param arch: architecture, should be '32' or '64'
-    :param targets: list of strings for targets, eg. ['arm', 'ppc']
-    """
-
-    xarch = 'x86_64'
-    if arch == '32':
-        xarch = 'i686'
-
-    cmd = [f"sudo docker exec -i {dockerId} sh -c "
-                                f"'cd $QEMU_SRC;echo $QEMU_SRC; pwd; ./configure --cross-prefix={xarch}-w64-mingw32- "
-                                         "--target-list=" + ','.join(targets) + "; make'"]
-
-    log('docker build: ' + ' '.join(cmd))
-    p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
-    p.wait()
-
-    cmd = [f"sudo docker exec -i {dockerId} sh -c 'mkdir ~/dlls{arch};  cd /usr/{xarch}-w64-mingw32/sys-root/mingw/bin/ ;"
-                                         f" cp *.dll ~/dlls{arch}'"]
-    log('docker cp dlls: ' + ' '.join(cmd))
-    p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
-    p.wait()
-
-    cmd = [f"sudo docker cp {dockerId}:/root/dlls{arch}/ {BIN_DIR}{arch}"]
-    log('docker cp root dlls: ' + ' '.join(cmd))
-    p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
-    p.wait()
-
-
-def _buildWinExecutables(arch, targets):
-    """
-    :param arch: architecture, should be '32' or '64'
-    :param targets: list of strings for targets, eg. ['arm', 'ppc']
-    """
-    log("---------Building: " + ','.join(targets) + f" for {arch}-bit Windows---------")
-    if os.path.exists(f"{BIN_DIR}{arch}"):
-        shutil.rmtree(f"{BIN_DIR}{arch}")
-    os.makedirs(f"{BIN_DIR}{arch}")
-
-    # start docker
-    start_docker_and_build(arch, targets)
-    dockerId = get_docker_id()
-
-    _dockerBuild(dockerId, arch, targets)
-    _copyFromDockerToSharedDir(dockerId, arch, targets)
-    check_dlls(arch, targets)
-    zip_files()
-    stop_docker()
+#def stop_docker():
+#    os.chdir('..')
+#    log(os.getcwd())
+#
+#    cmd = ["sudo docker stop " + id]
+#    log('Stop docker: ' + ' '.join(cmd))
+#    p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
+#
+#    p.wait()
+#
+#    # remove docker
+#    cmd = ['sudo rm -rf docker-src*']
+#    p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
+#
+#    os.chdir('isystem')
+#
+#
+#def _copyFromDockerToSharedDir(id, arch, targets):
+#    """
+#    :param arch: architecture, should be '32' or '64'
+#    :param targets: list of strings for targets, eg. ['arm', 'ppc']
+#    """
+#
+#    for target in targets:
+#        cmd = [f"sudo docker cp {id}:tmp/qemu-test/src/{target}-softmmu/qemu-system-{target}.exe"
+#                                        f" {BIN_DIR}{arch}"]
+#        p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
+#        p.wait()
+#
+#
+#def _dockerBuild(dockerId, arch: str, targets):
+#    """
+#    :param arch: architecture, should be '32' or '64'
+#    :param targets: list of strings for targets, eg. ['arm', 'ppc']
+#    """
+#
+#    xarch = 'x86_64'
+#    if arch == '32':
+#        xarch = 'i686'
+#
+#    cmd = [f"sudo docker exec -i {dockerId} sh -c "
+#                                f"'cd $QEMU_SRC;echo $QEMU_SRC; pwd; ./configure --cross-prefix={xarch}-w64-mingw32- "
+#                                         "--target-list=" + ','.join(targets) + "; make'"]
+#
+#    log('docker build: ' + ' '.join(cmd))
+#    p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
+#    p.wait()
+#
+#    cmd = [f"sudo docker exec -i {dockerId} sh -c 'mkdir ~/dlls{arch};  cd /usr/{xarch}-w64-mingw32/sys-root/mingw/bin/ ;"
+#                                         f" cp *.dll ~/dlls{arch}'"]
+#    log('docker cp dlls: ' + ' '.join(cmd))
+#    p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
+#    p.wait()
+#
+#    cmd = [f"sudo docker cp {dockerId}:/root/dlls{arch}/ {BIN_DIR}{arch}"]
+#    log('docker cp root dlls: ' + ' '.join(cmd))
+#    p = subprocess.Popen(cmd, shell=True, executable='/bin/bash')
+#    p.wait()
+#
+#
+#def _buildWinExecutables(arch, targets):
+#    """
+#    :param arch: architecture, should be '32' or '64'
+#    :param targets: list of strings for targets, eg. ['arm', 'ppc']
+#    """
+#    log("---------Building: " + ','.join(targets) + f" for {arch}-bit Windows---------")
+#    if os.path.exists(f"{BIN_DIR}{arch}"):
+#        shutil.rmtree(f"{BIN_DIR}{arch}")
+#    os.makedirs(f"{BIN_DIR}{arch}")
+#
+#    # start docker
+#    start_docker_and_build(arch, targets)
+#    dockerId = get_docker_id()
+#
+#    _dockerBuild(dockerId, arch, targets)
+#    _copyFromDockerToSharedDir(dockerId, arch, targets)
+#    check_dlls(arch, targets)
+#    zip_files()
+#    stop_docker()
 
 
 def parseArgs():
@@ -255,8 +253,11 @@ def main():
     for arch in args.arch:
         hostDir = create_host_dir()
         log('Host dir successfully created: ' + hostDir)
-        start_docker(hostDir, args.targets)
-        # _buildWinExecutables(arch, args.targets)
+        _docker_build(hostDir, args.targets)
+        # hostDir = 'docker-src.2019-09-11-15.06.09.6052'
+        _check_dlls(hostDir, '', [])
+        _zip_files(hostDir)
+        shutil.copy2(f'{ZIP_FILE_NAME}.{ZIP_FILE_EXT}', '/media/sf_kubuntu1704/')
 
 
 if __name__ == '__main__':
